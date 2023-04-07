13c3a78d2a2052e5f50e4909e3a953753e8ca965
daevidvo <itsdavidvo@gmail.com>
2023-04-06 20:49:07 -0700

    added .gitignore file for linkedin_login_info.py

--- script.py
+++ script.py
@@ -2,3 +2,5 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
